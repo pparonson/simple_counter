f94ef21880a08ce346f4161df38da9af7d0c8770
pparonson <preston.aronson@gmail.com>
2018-05-14 10:06:10 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const h = require('hyperscript')
+const { div, span, h1, p } =
+  require('hyperscript-helpers')(h);
+
+const appTitle = h1("Basic Minimal Webpack Config for New APP");
+const appDescription = p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"
+  + " eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
+  + " minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+  + " ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate"
+  + " velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat"
+  + " cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id"
+  + " est laborum."
+);
+
+console.log(appTitle.outerHTML);
+console.log(appDescription.outerHTML);
+
+const appTitleNode = document.getElementById('appTitle');
+const appDescriptionNode = document.getElementById('appDescription')
+appTitleNode.appendChild(appTitle);
+appDescriptionNode.appendChild(appDescription);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: ['./src/index.js'],
+  // devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  // watch: true,
+  // devServer: {
+  //   contentBase: './src',
+  //   compress: true,
+  //   port: 9000,
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+            // plugins: [require('babel-plugin-transform-object-rest-spread')],
+          },
+        },
+      },
+    ],
+  }
+};
