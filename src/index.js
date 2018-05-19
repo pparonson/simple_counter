3e52c0f9e2658461729baa71f8609ef4dd21dc0a
pparonson <preston.aronson@gmail.com>
2018-05-19 08:08:49 -0600

    create calorie counter table

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const h = require('hyperscript')
-const { div, span, h1, p } =
+const R = require("ramda");
+const h = require('hyperscript');
+const { h1, p, table, thead, tbody, tr, th, td } =
   require('hyperscript-helpers')(h);
 
 const appTitle = h1("Basic Minimal Webpack Config for New APP");
@@ -12,11 +13,74 @@ const appDescription = p("Lorem ipsum dolor sit amet, consectetur adipiscing eli
   + " est laborum."
 );
 
-console.log(appTitle.outerHTML);
-console.log(appDescription.outerHTML);
-console.log("Success");
+const MEALS = [
+  {description: "breakfast", calories: 460}
+  , {description: "snack", calories: 180}
+  , {description: "lunch", calories: 600}
+];
 
-const appTitleNode = document.getElementById('appTitle');
-const appDescriptionNode = document.getElementById('appDescription')
-appTitleNode.appendChild(appTitle);
-appDescriptionNode.appendChild(appDescription);
+function createCell(tag, className, value) {
+  // return hyperscript tag
+  return tag({className}, value);
+}
+
+function createMealRow(className, meal) {
+  // use array to pass multiple child fn nodes
+  return tr({className}, [
+    createCell(td, "pa2", meal.description)
+    , createCell(td, "pa2 tr", meal.calories)
+  ]);
+}
+
+const headerRow = tr([
+    createCell(td, "pa2", "MEALS")
+    , createCell(td, "pa2 tr", "CALORIES")
+  ]);
+
+function createTotalRow(className, meals) {
+  const total = R.compose(
+    getTotalCalories
+    , getCalories
+  );
+  return tr({className}, [
+    createCell(td, "pa2 tr", "TOTAL:")
+    , createCell(td, "pa2 tr", total(meals))
+  ]);
+}
+
+function createTHead(className) {
+  return th({className}, headerRow);
+}
+
+function createTbody(className, meals) {
+  const rows = R.map(R.partial(createMealRow, ["pa2 stripe-dark"])
+    , meals);
+  return tbody({className}, [
+    rows
+    , createTotalRow("bt b", meals)]);
+}
+
+function createTable(className) {
+  // use array to pass multiple child fn nodes
+  return table({className}, [
+    createTHead("")
+    , createTbody("", MEALS)
+  ]);
+}
+
+// helper, point-free: meals
+const getCalories = R.map(item => {
+  return item.calories;
+});
+
+// helper
+function sum(x, y) {
+  return x + y;
+}
+
+// helper, point-free: getCalories
+const getTotalCalories = R.reduce(sum, 0);
+
+// const node = document.getElementById('mealTable');
+// const view = createTable("mw5 w-100 center collapse", MEALS);
+// node.appendChild(view);
